94da35042fe53cc65520bb36e6af9b5a8db9c7e4
Sameerwaseem21 <sairwaseem2015@hotmail.com>
2022-03-22 13:54:50 +0200

    fix side space

---
+++ src/components/burgermenu/burgermenu.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BurgerMenu: React.FC = () => {
+
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <p>asd</p>
+            <p>asd2</p>
+            <p>asd3</p>
+        </div>
+    )
+}
+
+export default BurgerMenu;
--- src/components/page/main/main.tsx
+++ src/components/page/main/main.tsx
@@ -23,11 +23,12 @@ const Main: React.FC<MainProps> = () => {
       <div className={styles.txtDiv}>
         <h1 className={styles.cvintro}><br></br><br></br>Hi welcome to my Portfolio.For contacting me you can see down below.</h1>
       </div>
-      <div className={styles.container}>
-      <a href='mailto:sairwaseem2015@hotmail.com'><img src={pic1} alt='asd' /></a>
-      <a href='https://www.instagram.com/sameerwaseem21/?hl=en'><img src={pic2} alt='asd' /></a>
-      <a href='https://www.linkedin.com/in/sameer-waseem-28b768230/'><img src={pic3} alt='asd' /></a>
-    </div>
+
+      <div className={styles.homeicon}>
+        <a href='mailto:sairwaseem2015@hotmail.com'><img src={pic1} alt='asd' /></a>
+        <a href='https://www.instagram.com/sameerwaseem21/?hl=en'><img src={pic2} alt='asd' /></a>
+        <a href='https://www.linkedin.com/in/sameer-waseem-28b768230/'><img src={pic3} alt='asd' /></a>
+      </div>
 
 
 
